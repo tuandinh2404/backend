92a8d141df6f523266a118b9632207f0e88286e3
dinhtuan24 <tuanliz159132@gmail.com>
2025-09-15 11:05:19 +0700

    sua lan 12

--- src/routes/feedRoutes.js
+++ src/routes/feedRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const feedController = require("../controllers/feedController")
+const AuthMiddleWare = require("../middlewares/AuthMiddleWare");
 
-router.post("/posts", feedController.Posts)
-router.get("/posts/:postId", feedController.getPosts)
+router.post("/posts", AuthMiddleWare, feedController.Posts)
+router.get("/posts/:postId", AuthMiddleWare, feedController.getPosts)
 module.exports = router;
