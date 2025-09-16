08afd3f3745c2835143afff9d491d0508a05216e
dinhtuan24 <tuanliz159132@gmail.com>
2025-09-16 12:34:59 +0700

    sua lan 15

--- src/routes/feedRoutes.js
+++ src/routes/feedRoutes.js
@@ -4,7 +4,6 @@ const multer = require("multer");
 const upload = multer()
 const feedController = require("../controllers/feedController")
 const AuthMiddleWare = require("../middlewares/AuthMiddleWare");
-const multer = require("multer");
 
 router.post("/posts", AuthMiddleWare, upload.array("media"), feedController.Posts)
 router.get("/posts/:postId", AuthMiddleWare, feedController.getPosts)
