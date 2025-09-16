b786145d486eb3f00f3a1e29b12ed4286e1bdef5
dinhtuan24 <tuanliz159132@gmail.com>
2025-09-16 12:33:28 +0700

    sua lan 14

--- src/routes/feedRoutes.js
+++ src/routes/feedRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer()
 const feedController = require("../controllers/feedController")
 const AuthMiddleWare = require("../middlewares/AuthMiddleWare");
+const multer = require("multer");
 
-router.post("/posts", AuthMiddleWare, feedController.Posts)
+router.post("/posts", AuthMiddleWare, upload.array("media"), feedController.Posts)
 router.get("/posts/:postId", AuthMiddleWare, feedController.getPosts)
 module.exports = router;
