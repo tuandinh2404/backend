bad64b2cc907908496c77c019ee16df4ca25c7b5
dinhtuan24 <tuanliz159132@gmail.com>
2025-09-18 10:31:52 +0700

    sua lan 26

--- src/routes/feedRoutes.js
+++ src/routes/feedRoutes.js
@@ -8,6 +8,6 @@ const feedController = require("../controllers/feedController")
 const AuthMiddleWare = require("../middlewares/AuthMiddleWare");
 
 router.post("/posts", AuthMiddleWare, upload.array("media"), feedController.Posts)
-router.get("/posts", AuthMiddleWare, feedController.getPosts)
-router.get("/posts/:postId", AuthMiddleWare, feedController.getAllPosts)
+router.get("/posts", AuthMiddleWare, feedController.getAllPosts)
+router.get("/posts/:postId", AuthMiddleWare, feedController.getPosts)
 module.exports = router;
