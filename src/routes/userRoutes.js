6fa1346a42659240ba48da3549c26b44fc41d171
dinhtuan24 <tuanliz159132@gmail.com>
2025-10-09 09:39:46 +0700

    avatarrr

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -11,7 +11,7 @@ router.post("/check-uid", userController.checkUid);
 router.post("/check-email-login", userController.checkEmailLogin);
 router.post("/logout", userController.LogOut);
 router.get("/get-uid", userController.getUID)
-router.post("/upload-profile", upload.single("profileImage"), userController.uploadProfile)
+router.post("/upload-profile", AuthMiddleWare,upload.single("profileImage"), userController.uploadProfile)
 router.get("/getusers/:uid", AuthMiddleWare, userController.getUsers)
 
 module.exports = router;
