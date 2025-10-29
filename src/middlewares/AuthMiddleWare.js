01e163681baea725b1b5c32c3452825a54bfd821
dinhtuan24 <tuanliz159132@gmail.com>
2025-10-29 12:15:43 +0700

    sualaidDdd

--- src/middlewares/AuthMiddleWare.js
+++ src/middlewares/AuthMiddleWare.js
@@ -1,32 +1,33 @@
-const db = require("../config/db")
-const jwt = require("jsonwebtoken")
-const JWT_SECRET = process.env.JWT_SECRET
+const db = require("../config/db");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const AuthMiddleWare = async (req, res, next) => {
-    const authHeader = req.headers["authorization"]
-    if(!authHeader) {
-        return res.status(401).json({message:"Thieu Token"})
+  try {
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      return res.status(401).json({ message: "Thieu Token" });
     }
-    const token = authHeader.split(" ")[1]
-    if(!token) {
-        return res.status(401).json({message:"Token khong hop le"})
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Token khong hop le" });
     }
 
-    jwt.verify(token, JWT_SECRET, async (err, user) => {
-        if(err) {
-            return res.status(403).json({message: "Token khong hop le"})
-        }
-        try{
-            const result = await db.query(`SELECT id, uid, firstname, lastname, email FROM users WHERE id = $1`, [user.id])
-            if(result.rows.length === 0) {
-                return res.status(401).json({message: "User No Exist"})
-            }
-            req.user = result.rows[0]
-            next()
-        } catch(err) {
-            console.error(err)
-            res.status(500).json({message:"Loi server khi xac thuc"})
-        }
-    })
-}
-module.exports = AuthMiddleWare
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    const result = await db.query(
+      `SELECT id, uid, firstname, lastname, email FROM users WHERE id = $1`,
+      [decoded.id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(401).json({ message: "User No Exist" });
+    }
+
+    req.user = result.rows[0];
+    next();
+  } catch (err) {
+    console.error("Loi xac thuc Token:", err);
+    res.status(403).json({ message: "Token không hợp lệ hoặc đã hết hạn" });
+  }
+};
+module.exports = AuthMiddleWare;
