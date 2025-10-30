14350073f2467b947374b12c5348a0e80b64cdf4
dinhtuan24 <tuanliz159132@gmail.com>
2025-10-30 09:13:16 +0700

    redis

---
+++ src/util/RedisClient.js
@@ -0,0 +1,12 @@
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+    url: "redis://127.0.0.1:6379",
+})
+
+redisClient.on("connect", () => console.log("Redis client đang kết nối"));
+redisClient.on("error", (err) => console.log("Lỗi kết nối Redis:", err));
+
+redisClient.connect();
+
+module.exports = redisClient;
