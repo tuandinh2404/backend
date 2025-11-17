7d9a0c996a9bd3a4123aef34d1c893dd7900b075
dinhtuan24 <tuanliz159132@gmail.com>
2025-11-17 12:25:13 +0700

    followss

--- src/controllers/followController.js
+++ src/controllers/followController.js
@@ -46,3 +46,66 @@ exports.followUser = async (req, res) => {
         return res.status(500).json({ message: "Lỗi Server"});
     }
 }
+
+exports.getRelationshipStatus = async ( req, res) => {
+    const currentUserId = req.user.id;
+    const followingId = parseInt(req.params.userId);
+
+    try {
+        const iFollowResult = await db.query(
+            `SELECT 1 FROM follows
+            WHERE follower_id =$1 AND following_id =$2`,
+            [currentUserId, followingId]
+        );
+
+        const followMeReuslt = await db.query(
+            `SELECT 1 FROM follows
+            WHERE follower_id = $1 AND following_id =$2`,
+            [followingId, currentUserId]
+        );
+
+        let status = "none";
+
+        if( iFollowResult.rowCount > 0 && followMeReuslt.rowCount > 0 ) {
+            status = "mutual";
+        } else if(followMeReuslt.rowCount > 0) {
+            status = "followed";
+        };
+
+        return res.status(200).json({ 
+            status,
+            is_Following: iFollowResult.rowCount > 0,
+            is_Followed: followMeReuslt.rowCount > 0
+        });
+    } catch (error) {
+        console.error("Mối quan hệ lỗi:", error);
+        return res.status(500).json({ message: "Lỗi Server"});
+    }
+}
+
+exports.unfollowUser = async ( req, res) => {
+    const currentUserId = req.user.id;
+    const followingId = parseInt(req.params.userId);
+
+    try {
+        const result = await db.query(
+            `DELETE FROM follows
+            WHERE follower_id =$1 AND following_id =$2`,
+            [currentUserId, followingId]
+        )
+        if(result.rowCount === 0) {
+            return res.status(400).json({
+                status: "not_following",
+                message: "Bạn chưa theo dõi người này."
+            });
+        };
+
+        return res.status(200).json({
+            status: "unfollowed",
+            message: "Hủy theo dõi thành công."
+        })
+    } catch (error) {
+        console.error("Error unfollowing user:", error);
+        return res.status(500).json({ message: "Lỗi Server"});
+    }
+}
--- src/routes/followRoutes.js
+++ src/routes/followRoutes.js
@@ -4,5 +4,7 @@ const followController = require('../controllers/followController');
 const AuthMiddleWare = require("../middlewares/AuthMiddleWare");
 
 router.post('/follow/:userId', AuthMiddleWare, followController.followUser);
+router.get('/relationship-status/:userId', AuthMiddleWare, followController.getRelationshipStatus);
+router.delete('/unfollow/:userId', AuthMiddleWare, followController.unfollowUser);
 
 module.exports = router;
